c4efd6dfb093c9fd36d15604846c46344481d922
Angel <araozangel842@gmail.com>
2021-10-19 22:31:26 +0300

    Work in progress

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import AddNewPost from './components/newPost/AddNewPost';
 import HomeScreen from './screens/HomeScreen';
+import NewPostScreen from './screens/NewPostScreen';
 
 export default function App() {
-	return <HomeScreen />;
+	return <NewPostScreen />;
 }
-
+// <HomeScreen />
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
