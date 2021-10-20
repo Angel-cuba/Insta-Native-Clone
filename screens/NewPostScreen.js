5de5291887fbfbc7d12539eba5a48199ee6208db
Angel <araozangel842@gmail.com>
2021-10-20 22:21:31 +0300

    passing navigation and styling

--- screens/NewPostScreen.js
+++ screens/NewPostScreen.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, Platform, Appearance } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 import AddNewPost from '../components/newPost/AddNewPost';
 
-const NewPostScreen = () => {
+const NewPostScreen = ({ navigation }) => {
 	return (
-		<SafeAreaView style={{ backgroundColor: 'black', flex: 1 }}>
-			<AddNewPost />
+		<SafeAreaView style={[styles.safeAreaView, Platform.OS === 'android' && styles.android]}>
+			<AddNewPost navigation={navigation} />
 		</SafeAreaView>
 	);
 };
 
 export default NewPostScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	safeAreaView: {
+		backgroundColor: 'black',
+		color: 'black',
+		flex: 1,
+	},
+	android: {
+		// marginTop: 16,
+	},
+});
