eca4acc1b684924875be1c6a6b63bd81fa40e32b
Angel <araozangel842@gmail.com>
2021-10-20 22:23:01 +0300

    New post side in progress

--- components/newPost/FormikPost.js
+++ components/newPost/FormikPost.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as yup from 'yup';
-import { Formik } from 'formik';
+import { Formik, Form } from 'formik';
 import { Image } from 'react-native';
 import { TextInput } from 'react-native';
+import { Button, Divider } from 'react-native-elements';
+import validUrl from 'valid-url';
 
 const FormikSchema = yup.object().shape({
 	imageUrl: yup.string().url().required('A URL is required'),
@@ -13,20 +15,54 @@ const FormikSchema = yup.object().shape({
 const DEFAULT_IMAGE =
 	'https://res.cloudinary.com/dqaerysgb/image/upload/v1628766841/qpelx36hxluhbyfgcgdj.jpg';
 
-const FormikPost = () => {
+const FormikPost = ({ navigation }) => {
 	const [tockUrl, setTockUrl] = useState(DEFAULT_IMAGE);
 	return (
 		<Formik
 			initialValues={{ caption: '', imageUrl: '' }}
-			onSubmit={(values) => console.log(values)}
+			onSubmit={(values) => {
+				console.log(values);
+				navigation.goBack();
+			}}
 			validationSchema={FormikSchema}
+			validateOnMount={true}
 		>
 			{({ handleBlur, handleChange, handleSubmit, values, errors, isValid }) => (
 				<>
-					<View>
-						<Image />
+					<View style={{ margin: 20, justifyContent: 'space-between', flexDirection: 'row' }}>
+						<Image
+							source={{ uri: validUrl.isUri(tockUrl) ? tockUrl : DEFAULT_IMAGE }}
+							style={styles.image}
+						/>
+						<View style={{ flex: 1, marginLeft: 20 }}>
+							<TextInput
+								style={styles.inputStyle}
+								placeholder="Write a caption..."
+								placeholderTextColor="white"
+								multiline={true}
+								onChangeText={handleChange('caption')}
+								onBlur={handleBlur('caption')}
+								value={values.caption}
+							/>
+						</View>
 					</View>
-					<TextInput placeholder="Hello" placeholderTextColor="white" />
+					<Divider width={0.2} orientation="vertical" />
+					<TextInput
+						onChange={(e) => setTockUrl(e.nativeEvent.text)}
+						style={styles.inputStyle}
+						placeholder="Enter image Url"
+						placeholderTextColor="white"
+						onChangeText={handleChange('imageUrl')}
+						onBlur={handleBlur('imageUrl')}
+						value={values.imageUrl}
+					/>
+					{errors.imageUrl && (
+						<Text style={{ fontWeight: 'bold', fontSize: 12, color: 'red' }}>
+							{errors.imageUrl}
+						</Text>
+					)}
+
+					<Button onPress={handleSubmit} title="Share" disabled={!isValid} />
 				</>
 			)}
 		</Formik>
@@ -35,4 +71,14 @@ const FormikPost = () => {
 
 export default FormikPost;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	image: {
+		width: 100,
+		height: 100,
+	},
+	inputStyle: {
+		color: 'white',
+		fontSize: 20,
+		// width: '70%',
+	},
+});
